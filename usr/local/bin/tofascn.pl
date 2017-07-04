5a20fbef5833ddb51a25d3cbc6a8735ffd28cfd2
U-CERTIPATH\Bob.Fontana <Bob.Fontana@CERTIPATH011.certipath.local>
2017-07-04 11:28:28 -0400

    First commit of utilities

---
+++ usr/local/bin/tofascn.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+#
+# tofascn.pl v1.4
+# vim: ts=2 nowrap
+#
+#
+# Usage: ./tofascn.pl <FASC-N in 200-bit format> 
+#
+# Converts 200-bit format FASC-N to human-readble form
+# 
+use strict;
+use warnings;
+use FindBin;
+use lib $FindBin::Bin . '/../lib/perl/lib';
+use LogParser qw(&cook);
+
+# Check args
+
+if (@ARGV < 1) {
+	die "Usage: $0 <raw FASC-N>\n";
+}
+
+my $length = length $ARGV[0];
+die "Invalid input length ($length)\n" if (length $ARGV[0] != 50);
+
+my $fascn = cook($ARGV[0]);
+
+$length = length $fascn;
+die "Invalid output length ($fascn)\n" if (length $fascn != 32);
+
+print "$fascn\n";
+
+exit 0;
---
+++ usr/local/lib/perl/lib/LogParser.pm
@@ -0,0 +1,134 @@
+package LogParser;
+# LogParser.pm v1.4
+
+use strict;
+use warnings;
+use Exporter;
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+
+$VERSION     = 1.00;
+@ISA         = qw(Exporter);
+@EXPORT      = ();
+@EXPORT_OK   = qw(cook clean output_value tlvparse);
+%EXPORT_TAGS = ( DEFAULT => [qw(&cook)],
+                 All     => [qw(&cook &clean &output_value &tlvparse)]);
+
+#
+# Deconstructs a 200-bit raw FASC-N to cooked form.
+#
+
+sub cook($) {
+	my $raw = shift;
+	my @hexdigits = ();
+	my $value;
+	my @bits = ();
+	my $bctr;
+	my $bitstr = "";
+	my $digits = "";
+	my $length;
+
+	@hexdigits = unpack "(A)*", $raw; 
+
+	# Convert each hex digit to 4 0's and 1's and concatenate to into a string.
+
+	map { $bitstr .= sprintf("%04b", hex($_)); } @hexdigits;
+
+	# Create a bit array so we can read 5 bits at a time.
+
+	@bits = split //, $bitstr;
+
+	for ($length = scalar @bits, $value = 0, $bctr = 0; $bctr < $length - 5; $bctr++) {
+
+		# The bits of each digit are reversed, with a trailing parity bit
+
+		$value |= ((($bits[$bctr] == 1) ? 0x01 : 0x00) << ($bctr % 5));
+
+		# Peek if we've done 5 bits, process, and roll over to another digit
+
+		if (($bctr + 1) % 5 == 0) { 
+			# Strip off high order (parity bit)
+			$value = $value & 0xF; 
+			# All of the field separators, etc., are > 9 so
+			$digits .= $value if ($value < 10);
+			# Ready for next digit
+			$value = 0;
+		}
+	}
+
+	return $digits
+}
+
+#
+# Cleans a line of ASCII hex of spaces or colons
+#
+
+sub clean($) {
+	my $line = shift;
+	$line =~ s/:|\s//g;
+	return $line;
+}
+
+#
+# Prints out the value and writes it to the specified file 
+#
+
+sub output_value($$$$$$$) {
+	my $start = shift;
+	my $len = shift;
+	my $name = shift;
+	my $charsref = shift;
+	my $octets = shift;
+	my $hashableref = shift;
+	my $base64cons = shift;
+	my $end = $start + $len;
+	my $outfile;
+	my $encoded;
+	my @chars = @$charsref;
+	my $hashable = $hashableref;
+
+	return if ($len == 0);
+
+	open $outfile, ">$name" . ".bin" or die "$outfile" . ".bin" . ": $!";
+	my $i;
+	for ($i = $start; $i < $end; $i++) {
+		if (defined $hashableref && defined $$hashable) {
+			$$hashable .= chr($chars[$i]);
+		}
+		print $outfile chr($chars[$i]);
+	}
+
+	close $outfile;	
+
+	# So that we can pipe this directly in to openssl asn1parse -inform pem ...
+
+	if ($base64cons == 1) {
+		$encoded = MIME::Base64::encode (substr ($octets, $start, $len));
+		print $encoded, "\n";
+	}
+}
+
+#
+# Parses a TLV, returning the tag, length, and value
+#
+
+sub tlvparse ($) {
+	my $charsref = shift;
+	my @chars = @$charsref;
+	my $len = 0;
+	my $tag = $chars[0];
+	my $lenbytes;
+	shift @chars;
+	if ($chars[0] & 0x80) {
+		$lenbytes = $chars[0] & 0x7F;
+		for (my $j = 0, $len = 0; $j < $lenbytes; $j++) {
+			shift @chars;
+			$len = (($len << 8) | $chars[0]);
+		}
+	} else {
+		$len = $chars[0];
+	}
+	shift @chars;
+	return ($tag, $len, @chars);
+}
+
+1;
